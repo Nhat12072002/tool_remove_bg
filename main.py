54e4f28e5c0298af51fae60fcb2db13d4b340f19
Nhat1207 <minhnhatnguyenphan1207@gmail.com>
2025-04-09 16:04:07 +0700

    choose img to remove bg

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from rembg import remove
-from PIL import Image
-input_path = '1.jpg'
-output_path = '1.png'
-input=Image.open(input_path)
-output=remove(input)
-output.save(output_path)
-Image.open(output_path)
+from background_remove import BackgroundRemover
+
+def main():
+    remover = BackgroundRemover()
+    if remover.choose_image():
+        remover.remove_background()
+
+if __name__ == "__main__":
+    main()
